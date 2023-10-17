d92a867af688007208546462cd2a8162a0754d38
Tala <70854429+Tala-cmd@users.noreply.github.com>
2023-10-17 22:39:10 +0300

    Navigation Menubar

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
 import './style.css'
+
+const navButton = document.querySelector('#menu');
+const menuBar = document.querySelector('[role="menubar"]');
+
+navButton.addEventListener('click', () => {
+    const isExpanded = JSON.parse(navButton.getAttribute('aria-expanded'));
+    navButton.setAttribute('aria-expanded', !isExpanded);
+    menuBar.classList.toggle('hidden');
+    menuBar.classList.toggle('flex');
+})
